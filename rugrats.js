77576fef934901fc2f4c6029533ea382ccfbda1c
deandremharrison <deandremharrison@gmail.com>
2018-05-23 16:22:18 -0400

    fix objects and questionDiv

--- rugrats.js
+++ rugrats.js
@@ -1,41 +1,343 @@
 
-// alert("does this work");
 
 
-function getQuestion() {
-    document.getElementById('questionDiv').style.display= 'block';
-    $('#ques').html("question1.question");
-}
- 
 
+// function moveChar() {
 
-function moveChar() {
-    var elem = document.getElementById("quesChar");   
-    var pos = 0;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (pos == 350) {
-        clearInterval(id);
-      } else {
-        pos++; 
-        elem.style.right = pos + 'px'; 
-      }
-    }
-  }
+//   }
 
-//  var question1 = {
-//     question: "Is this the first question?",
-//     ans1: "Maybe",
-//     ans2: "Yes",
-//     ans3: "No",
-//     answer: 2
+// $('#id').click(function getUsername(){
+//   $('.lpInput').val();
+// })
+
+// function getUsername() {
+//     document.getElementById("lpInput").value;
+//     console.log(un)
+// $(document).ready(function(un){
+//     $('').append(un);
+//     console.log($("#username"))
+// })
 // }
-  
-function getUsername() {
-    var un = document.getElementById("lpInput").value;
-    console.log(un)
-    $(document).ready(function(un){
-        $('').append(un);
-        console.log($("#username"))
-    })
+
+var cat1_1 = {
+  question: "Is this the first question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat1.100",
+  pointVal: 100,
+  answer: [2]
 }
+
+
+$('#r1c1').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat1_1.pointVal);
+  $('#ques').html(cat1_1.question);
+  $('#firstBttn').html(cat1_1.ans1);
+  $('#secondBttn').html(cat1_1.ans2);
+  $('#thirdBttn').html(cat1_1.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat2_1 = {
+  question: "Is this the Second question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat2.100",
+  pointVal: 100,
+  answer: [2]
+}
+
+$('#r1c2').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat2_2.pointVal);
+  $('#ques').html(cat2_1.question);
+  $('#firstBttn').html(cat2_1.ans1);
+  $('#secondBttn').html(cat2_1.ans2);
+  $('#thirdBttn').html(cat2_1.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat3_1 = {
+  question: "Is this the third question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat3.100",
+  pointVal: 100,
+  answer: [2]
+}
+
+$('#r1c3').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat3_2.pointVal);
+  $('#ques').html(cat3_1.question);
+  $('#firstBttn').html(cat3_1.ans1);
+  $('#secondBttn').html(cat3_1.ans2);
+  $('#thirdBttn').html(cat3_1.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat4_1 = {
+  question: "Is this the fourth question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat4.100",
+  pointVal: 100,
+  answer: [2]
+}
+
+$('#r1c4').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat4_1.pointVal);
+  $('#ques').html(cat4_1.question);
+  $('#firstBttn').html(cat4_1.ans1);
+  $('#secondBttn').html(cat4_1.ans2);
+  $('#thirdBttn').html(cat4_1.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat1_2 = {
+  question: "What is the name of Angelica's cat?",
+  ans1: "Fluffy",
+  ans2: "Snowflake",
+  ans3: "Princess",
+  id: "cat1.200",
+  pointVal: 200,
+  answer: "#firstBttn"
+}
+
+$('#r2c1').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat1_2.pointVal);
+  $('#ques').html(cat1_2.question);
+  $('#firstBttn').html(cat1_2.ans1);
+  $('#secondBttn').html(cat1_2.ans2);
+  $('#thirdBttn').html(cat1_2.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat2_2 = {
+  question: "Is this the fourth question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat2.200",
+  pointVal: 200,
+  answer: [2]
+}
+
+$('#r2c2').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat2_2.pointVal);
+  $('#ques').html(cat2_2.question);
+  $('#firstBttn').html(cat2_2.ans1);
+  $('#secondBttn').html(cat2_2.ans2);
+  $('#thirdBttn').html(cat2_2.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat3_2 = {
+  question: "Is this the fourth question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat3.200",
+  pointVal: 200,
+  answer: [2]
+}
+
+$('#r2c3').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat3_2.pointVal);
+  $('#ques').html(cat3_2.question);
+  $('#firstBttn').html(cat3_2.ans1);
+  $('#secondBttn').html(cat3_2.ans2);
+  $('#thirdBttn').html(cat3_2.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat4_2 = {
+  question: "Is this the fourth question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat4.200",
+  pointVal: 200,
+  answer: [2]
+}
+
+$('#r2c4').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat4_2.pointVal);
+  $('#ques').html(cat4_2.question);
+  $('#firstBttn').html(cat4_2.ans1);
+  $('#secondBttn').html(cat4_2.ans2);
+  $('#thirdBttn').html(cat4_2.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat1_3 = {
+  question: "What does Angelica get from the Tooth Fairy for her tooth?",
+  ans1: "A penny",
+  ans2: "A note",
+  ans3: "A dime",
+  id: "cat1.400",
+  pointVal: 400,
+  answer: "#thirdBttn"
+}
+
+
+$('#r3c1').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat1_3.pointVal);
+  $('#ques').html(cat1_3.question);
+  $('#firstBttn').html(cat1_3.ans1);
+  $('#secondBttn').html(cat1_3.ans2);
+  $('#thirdBttn').html(cat1_3.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat2_3 = {
+  question: "Is this the fourth question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat2.400",
+  pointVal: 400,
+  answer: [2]
+}
+
+$('#r3c2').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat2_3.pointVal);
+  $('#ques').html(cat2_3.question);
+  $('#firstBttn').html(cat2_3.ans1);
+  $('#secondBttn').html(cat2_3.ans2);
+  $('#thirdBttn').html(cat2_3.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat3_3 = {
+  question: "Is this the fourth question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat3.400",
+  pointVal: 400,
+  answer: [2]
+}
+
+$('#r3c3').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat3_3.pointVal);
+  $('#ques').html(cat3_3.question);
+  $('#firstBttn').html(cat3_3.ans1);
+  $('#secondBttn').html(cat3_3.ans2);
+  $('#thirdBttn').html(cat3_3.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat4_3 = {
+  question: "Is this the fourth question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat4.400",
+  pointVal: 400,
+  answer: [2]
+}
+
+$('#r3c4').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat4_3.pointVal);
+  $('#ques').html(cat4_3.question);
+  $('#firstBttn').html(cat4_3.ans1);
+  $('#secondBttn').html(cat4_3.ans2);
+  $('#thirdBttn').html(cat4_3.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat1_4 = {
+  question: "Angelica diagnoses Chuckie with what illness?",
+  ans1: "Clownophobia",
+  ans2: "Rhinoceritis",
+  ans3: "Reptar fever",
+  id: "cat1.800",
+  pointVal: 800,
+  answer: "#secondBttn"
+}
+
+
+$('#r4c1').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat1_4.pointVal);
+  $('#ques').html(cat1_4.question);
+  $('#firstBttn').html(cat1_4.ans1);
+  $('#secondBttn').html(cat1_4.ans2);
+  $('#thirdBttn').html(cat1_4.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat2_4 = {
+  question: "Is this the fourth question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat2.800",
+  pointVal: 800,
+  answer: [2]
+}
+
+$('#r4c2').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat2_4.pointVal);
+  $('#ques').html(cat2_4.question);
+  $('#firstBttn').html(cat2_4.ans1);
+  $('#secondBttn').html(cat2_4.ans2);
+  $('#thirdBttn').html(cat2_4.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat3_4 = {
+  question: "Is this the fourth question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat3.800",
+  pointVal: 800,
+  answer: [2]
+}
+
+$('#r4c3').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat3_4.pointVal);
+  $('#ques').html(cat3_4.question);
+  $('#firstBttn').html(cat3_4.ans1);
+  $('#secondBttn').html(cat3_4.ans2);
+  $('#thirdBttn').html(cat3_4.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
+
+var cat4_4 = {
+  question: "Is this the fourth question?",
+  ans1: "Maybe",
+  ans2: "Yes",
+  ans3: "No",
+  id: "cat4.800",
+  pointVal: 800,
+  answer: [2]
+}
+
+$('#r4c4').click(function () {
+  $('.questionDiv').css('display', 'block');
+  $('.amount').html(cat4_4.pointVal);
+  $('#ques').html(cat4_4.question);
+  $('#firstBttn').html(cat4_4.ans1);
+  $('#secondBttn').html(cat4_4.ans2);
+  $('#thirdBttn').html(cat4_4.ans3);
+  $('#charDiv').animate({marginRight:"200px"}, 800);
+})
