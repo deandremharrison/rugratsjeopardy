07f0de3d0ad8991dce394cf08536f64c8e59ed72
deandremharrison <deandremharrison@gmail.com>
2018-05-23 10:05:48 -0400

    add jquery label

--- rugrats.js
+++ rugrats.js
@@ -1,7 +1,41 @@
+
 // alert("does this work");
+
 
 function getQuestion() {
     document.getElementById('questionDiv').style.display= 'block';
-    
+    $('#ques').html("question1.question");
 }
  
+
+
+function moveChar() {
+    var elem = document.getElementById("quesChar");   
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.right = pos + 'px'; 
+      }
+    }
+  }
+
+//  var question1 = {
+//     question: "Is this the first question?",
+//     ans1: "Maybe",
+//     ans2: "Yes",
+//     ans3: "No",
+//     answer: 2
+// }
+  
+function getUsername() {
+    var un = document.getElementById("lpInput").value;
+    console.log(un)
+    $(document).ready(function(un){
+        $('').append(un);
+        console.log($("#username"))
+    })
+}
